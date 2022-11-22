0fdbf8bc497b5ee93d8084c055d1378aab75be4a
kdiawara96 <karimdiawara96@gmail.com>
2022-11-22 16:26:28 +0000

    first commit

---
+++ src/main/java/secuCom/ml/secu_Com/SecuComApplication.java
@@ -0,0 +1,13 @@
+package secuCom.ml.secu_Com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuComApplication.class, args);
+	}
+
+}
